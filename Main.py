2bf5a9e318e79e8d0aeb768b5c690e30b1014ec6
Shashank Sharma <shashankrnr32@gmail.com>
2018-10-22 21:32:37 +0530

    2132_22102018

--- Main.py
+++ Main.py
@@ -4,7 +4,11 @@ Contributors :
 
 Main File : Run this file to Create Batch Certificates
 '''
+import sys
+import os
 
+sys.path.insert(0,os.getcwd()+'\Scripts')
 import BatchCertificateMaker as cert
+
 
 cert.createBatchCertificates()
